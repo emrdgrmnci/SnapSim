ad92fd605a4c921cb713a8cb472232aa6b3c2c71
Emre Değirmenci <degirmenci.a.emre@gmail.com>
2025-11-29 14:10:47 +0300

    Initial Commit

---
+++ SnapSim/SnapSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSimApp.swift
+//  SnapSim
+//
+//  Created by Emre Değirmenci on 29.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SnapSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
